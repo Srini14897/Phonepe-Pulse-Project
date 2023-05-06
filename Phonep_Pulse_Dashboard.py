ce5f9d783440d4f562477ce2765eb257d5230c4b
srinivas <112272390+Srini14897@users.noreply.github.com>
2023-05-07 01:43:56 +0530

    Update Phonep_Pulse_Dashboard.py

--- Phonep_Pulse_Dashboard.py
+++ Phonep_Pulse_Dashboard.py
@@ -15,14 +15,14 @@ st. set_page_config(layout="wide")
 
 #connect like below if you deploy through Git
 # DATASETS
-Data_Aggregated_Transaction_df= pd.read_csv(r'Phonepe-Pulse-Project/data/Aggregate_transaction_table.csv')
-Data_Aggregated_User_Summary_df= pd.read_csv(r'Phonepe-Pulse-Project/data/Data_Aggregated_User_Summary_Table.csv')
-Data_Aggregated_User_df= pd.read_csv(r'Phonepe-Pulse-Project/data/Data_Aggregated_User_Table.csv')
-Scatter_Geo_Dataset =  pd.read_csv(r'Phonepe-Pulse-Project/data/Data_Map_Districts_Longitude_Latitude.csv')
-Coropleth_Dataset =  pd.read_csv(r'Phonepe-Pulse-Project/data/Data_Map_IndiaStates_TU.csv')
-Data_Map_Transaction_df = pd.read_csv(r'Phonepe-Pulse-Project/data/Data_Map_Transaction_Table.csv')
-Data_Map_User_Table= pd.read_csv(r'Phonepe-Pulse-Project/data/Data_Map_User_Table.csv')
-Indian_States= pd.read_csv(r'Phonepe-Pulse-Project/data/Longitude_Latitude_State_Table.csv')
+Data_Aggregated_Transaction_df= pd.read_csv(r'data/Aggregate_transaction_table.csv')
+Data_Aggregated_User_Summary_df= pd.read_csv(r'data/Data_Aggregated_User_Summary_Table.csv')
+Data_Aggregated_User_df= pd.read_csv(r'data/Data_Aggregated_User_Table.csv')
+Scatter_Geo_Dataset =  pd.read_csv(r'data/Data_Map_Districts_Longitude_Latitude.csv')
+Coropleth_Dataset =  pd.read_csv(r'data/Data_Map_IndiaStates_TU.csv')
+Data_Map_Transaction_df = pd.read_csv(r'data/Data_Map_Transaction_Table.csv')
+Data_Map_User_Table= pd.read_csv(r'data/Data_Map_User_Table.csv')
+Indian_States= pd.read_csv(r'data/Longitude_Latitude_State_Table.csv')
 
 tab1 = st.tabs(["PhonePe Pulse | THE BEAT OF PROGRESS"])
 colT1,colT2 = st.columns([2,8])
